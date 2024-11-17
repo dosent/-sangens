ea42f81821871faa9c2738bbc0b0a0060603aa01
Ivan Neshin <ivan.neshin@simbirsoft.com>
2024-11-17 13:56:16 +0400

    -

--- custom_components/sangens/__init__.py
+++ custom_components/sangens/__init__.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
 DOMAIN = 'sangens_station'
 
-
-def setup(hass, config):
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     hass.states.set("sangens_station.world", "Paulus")
 
     return True
