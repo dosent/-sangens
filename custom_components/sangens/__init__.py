ee693634b612c1efdb3fc86daea2fcba7d011cef
Ivan Neshin <ivan.neshin@simbirsoft.com>
2024-11-17 13:50:03 +0400

    -

--- custom_components/sangens/__init__.py
+++ custom_components/sangens/__init__.py
@@ -1,14 +1,7 @@
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_TOKEN
-from homeassistant.core import ServiceCall
-
-from . import utils
-
 DOMAIN = 'sangens_station'
 
 
-def setup(hass, hass_config):
-    config = hass_config[DOMAIN]
-
-    filename = hass.config.path(f".{DOMAIN}.txt")
+def setup(hass, config):
+    hass.states.set("sangens_station.world", "Paulus")
 
     return True
